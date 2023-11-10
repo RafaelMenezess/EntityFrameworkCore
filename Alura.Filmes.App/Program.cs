fa894c9e0c1d5f56ba3b28892ed4b39f6b62ac3a
Rafael Menezes <rafa.rcm@hotmail.com>
2023-11-09 21:22:36 -0300

    Mapeando Atores

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Alura.Filmes.App
+﻿using System;
+using Alura.Filmes.App.Dados;
+
+namespace Alura.Filmes.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            using (var contexto = new AluraFilmesContexto())
+            {
+                foreach (var ator in contexto.Atores)
+                {
+                    Console.WriteLine(ator);
+                }
+            }
         }
     }
 }
