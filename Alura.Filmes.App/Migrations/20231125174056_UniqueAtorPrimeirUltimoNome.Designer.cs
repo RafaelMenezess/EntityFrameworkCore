f5a43a469b38e04562581655b906f3332be7880c
Rafael Menezes <rafa.rcm@hotmail.com>
2023-11-25 14:48:33 -0300

    Confirgurando restrições Unique

--- Alura.Filmes.App/Migrations/20231125174056_UniqueAtorPrimeirUltimoNome.Designer.cs
+++ Alura.Filmes.App/Migrations/20231125174056_UniqueAtorPrimeirUltimoNome.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Alura.Filmes.App.Migrations
 {
     [DbContext(typeof(AluraFilmesContexto))]
-    partial class AluraFilmesContextoModelSnapshot : ModelSnapshot
+    [Migration("20231125174056_UniqueAtorPrimeirUltimoNome")]
+    partial class UniqueAtorPrimeirUltimoNome
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Alura.Filmes.App/Migrations/20231125174056_UniqueAtorPrimeirUltimoNome.cs
@@ -0,0 +1,105 @@
+﻿using Alura.Filmes.App.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Alura.Filmes.App.Migrations
+{
+    public partial class UniqueAtorPrimeirUltimoNome : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_update",
+                table: "language",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "getdate()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_update",
+                table: "film_actor",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "getdate()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_update",
+                table: "film",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_update",
+                table: "actor",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "getdate()");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_actor_first_name_last_name",
+                table: "actor",
+                columns: new[] { "first_name", "last_name" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_actor_first_name_last_name",
+                table: "actor");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_update",
+                table: "language",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "getdate()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_update",
+                table: "film_actor",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "getdate()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_update",
+                table: "film",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_update",
+                table: "actor",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "getdate()");
+        }
+    }
+}
