45e919a89ff155363fadeb8a61925ddb9b64a047
Rafael Menezes <rafa.rcm@hotmail.com>
2023-12-09 15:28:43 -0300

    Comando Insert e Delete

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -15,26 +15,37 @@ namespace Alura.Filmes.App
         {
             using (var contexto = new AluraFilmesContexto())
             {
-                var categ = "Action";
-                var paramCateg = new SqlParameter("category_name", categ);
-                var paramTotal = new SqlParameter
-                {
-                    ParameterName = "@total_actors",
-                    Size = 4,
-                    Direction = System.Data.ParameterDirection.Output
-                };
+                var sql = "INSERT INTO language (name) VALUES ('Teste 1'), ('Teste 1'), ('Teste 1')";
+                var registros = contexto.Database.ExecuteSqlCommand(sql);
+                Console.WriteLine($"O total de registros afetados é {registros}.");
 
-                contexto
-                    .Database
-                    .ExecuteSqlCommand("total_actors_from_given_category @category_name, @total_actors OUT", 
-                                        paramCateg, 
-                                        paramTotal);
-
-                Console.WriteLine($"O total de atores na categoria {categ} é de {paramTotal.Value}.");
-
+                var deletSql = "DELETE FROM language WHERE name LIKE 'Teste%'";
+                registros = contexto.Database.ExecuteSqlCommand(deletSql);
+                Console.WriteLine($"O total de registros afetados é {registros}.");
             }
 
             Console.ReadKey();
+        }
+
+
+        private static void StoredProcedure(AluraFilmesContexto contexto)
+        {
+            var categ = "Action";
+            var paramCateg = new SqlParameter("category_name", categ);
+            var paramTotal = new SqlParameter
+            {
+                ParameterName = "@total_actors",
+                Size = 4,
+                Direction = System.Data.ParameterDirection.Output
+            };
+
+            contexto
+                .Database
+                .ExecuteSqlCommand("total_actors_from_given_category @category_name, @total_actors OUT",
+                                    paramCateg,
+                                    paramTotal);
+
+            Console.WriteLine($"O total de atores na categoria {categ} é de {paramTotal.Value}.");
         }
     }
 }
